90c9c00ca72f336a4156e2b7f0b34e3e9be1cc8e
missxiaolin <462441355@qq.com>
2024-12-30 22:19:29 +0800

    feat: 还有 3 个 json明天继续

--- mic-vie/src/components/formula/config.js
+++ mic-vie/src/components/formula/config.js
@@ -188,7 +188,266 @@ export const collapseList = [
   },
   {
     title: "数学函数",
-    children: [],
+    children: [
+      {
+        title: "ABS",
+        doc: "ABS(num)",
+        desc: "返回传入数字的绝对值。",
+        children: [
+          {
+            name: "num",
+            type: "number",
+            desc: "数值",
+          },
+        ],
+      },
+      {
+        title: "MIN",
+        doc: "MIN(num1, num2, ...numN)",
+        desc: "获取最小值，如果只有一个参数且是数组，则计算这个数组内的值。",
+        children: [
+          {
+            name: "num",
+            type: "...number",
+            desc: "数值",
+          },
+        ],
+      },
+      {
+        title: "MAX",
+        doc: "MAX(num1, num2, ...numN)",
+        desc: "获取最大值，如果只有一个参数且是数组，则计算这个数组内的值。",
+        children: [
+          {
+            name: "num",
+            type: "...number",
+            desc: "数值",
+          },
+        ],
+      },
+      {
+        title: "MAX",
+        doc: "SUM(num1, num2, ...numN)",
+        desc: "求和，如果只有一个参数且是数组，则计算这个数组内的值。",
+        children: [
+          {
+            name: "num",
+            type: "...number",
+            desc: "数值",
+          },
+        ],
+      },
+      {
+        title: "INT",
+        doc: "INT(num)",
+        desc: "将数值向下取整为最接近的整数。",
+        children: [
+          {
+            name: "num",
+            type: "number",
+            desc: "数值",
+          },
+        ],
+      },
+      {
+        title: "MOD",
+        doc: "MOD(num, divisor)",
+        desc: "将返回两数相除的余数，参数 number 是被除数，divisor 是除数。",
+        children: [
+          {
+            name: "num",
+            type: "number",
+            desc: "被除数",
+          },
+          {
+            name: "divisor",
+            type: "number",
+            desc: "除数",
+          },
+        ],
+      },
+      {
+        title: "PI",
+        doc: "PI()",
+        desc: "圆周率 3.1415...。",
+        children: [],
+      },
+      {
+        title: "ROUND",
+        doc: "ROUND(num[, numDigits = 2])",
+        desc: "将数字四舍五入到指定的位数，可以设置小数位。",
+        children: [
+          {
+            name: "num",
+            type: "number",
+            desc: "要处理的数字",
+          },
+          {
+            name: "numDigits",
+            type: "number",
+            desc: "小数位数，默认为2",
+          },
+        ],
+      },
+      {
+        title: "FLOOR",
+        doc: "FLOOR(num[, numDigits=2])",
+        desc: "将数字向下取整到指定的位数，可以设置小数位。",
+        children: [
+          {
+            name: "num",
+            type: "number",
+            desc: "要处理的数字",
+          },
+          {
+            name: "numDigits",
+            type: "number",
+            desc: "小数位数，默认为2",
+          },
+        ],
+      },
+      {
+        title: "CEIL",
+        doc: "CEIL(num[, numDigits=2])",
+        desc: "将数字向上取整到指定的位数，可以设置小数位。",
+        children: [
+          {
+            name: "num",
+            type: "number",
+            desc: "要处理的数字",
+          },
+          {
+            name: "numDigits",
+            type: "number",
+            desc: "小数位数，默认为2",
+          },
+        ],
+      },
+      {
+        title: "SQRT",
+        doc: "SQRT(num)",
+        desc: "开平方，参数 number 为非负数",
+        children: [
+          {
+            name: "num",
+            type: "number",
+            desc: "要处理的数字",
+          },
+        ],
+      },
+      {
+        title: "AVG",
+        doc: "AVG(num1, num2, ...numN)",
+        desc: "返回所有参数的平均值，如果只有一个参数且是数组，则计算这个数组内的值。",
+        children: [
+          {
+            name: "num",
+            type: "...number",
+            desc: "要处理的数字",
+          },
+        ],
+      },
+      {
+        title: "DEVSQ",
+        doc: "DEVSQ(num1, num2, ...numN)",
+        desc: "返回数据点与数据均值点之差（数据偏差）的平方和，如果只有一个参数且是数组，则计算这个数组内的值。",
+        children: [
+          {
+            name: "num",
+            type: "...number",
+            desc: "要处理的数字",
+          },
+        ],
+      },
+      {
+        title: "AVEDEV",
+        doc: "AVEDEV(num1, num2, ...numN)",
+        desc: "数据点到其算术平均值的绝对偏差的平均值。",
+        children: [
+          {
+            name: "num",
+            type: "...number",
+            desc: "要处理的数字",
+          },
+        ],
+      },
+      {
+        title: "HARMEAN",
+        doc: "HARMEAN(num1, num2, ...numN)",
+        desc: "数据点的调和平均值，如果只有一个参数且是数组，则计算这个数组内的值。",
+        children: [
+          {
+            name: "num",
+            type: "...number",
+            desc: "要处理的数字",
+          },
+        ],
+      },
+      {
+        title: "LARGE",
+        doc: "LARGE(array, k)",
+        desc: "数据集中第 k 个最大值。",
+        children: [
+          {
+            name: "nums",
+            type: "array",
+            desc: "要处理的数字",
+          },
+          {
+            name: "k",
+            type: "number",
+            desc: "第几大",
+          },
+        ],
+      },
+      {
+        title: "UPPERMONEY",
+        doc: "UPPERMONEY(num)",
+        desc: "将数值转为中文大写金额。",
+        children: [
+          {
+            name: "num",
+            type: "number",
+            desc: "要处理的数字",
+          },
+        ],
+      },
+      {
+        title: "RAND",
+        doc: "RAND()",
+        desc: "返回大于等于 0 且小于 1 的均匀分布随机实数。每一次触发计算都会变化。 示例：`RAND()*100`， 返回 0-100 之间的随机数。",
+        children: [],
+      },
+      {
+        title: "LAST",
+        doc: "LAST(array)",
+        desc: "取数据最后一个。",
+        children: [
+          {
+            name: "arr",
+            type: "...number",
+            desc: "要处理的数组",
+          },
+        ],
+      },
+      {
+        title: "POW",
+        doc: "POW(base, exponent)",
+        desc: "返回基数的指数次幂，参数base为基数，exponent为指数，如果参数值不合法则返回基数本身，计算结果不合法，则返回NaN。",
+        children: [
+          {
+            name: "base",
+            type: "number",
+            desc: "基数",
+          },
+          {
+            name: "exponent",
+            type: "number",
+            desc: "指数",
+          },
+        ],
+      },
+    ],
   },
   {
     title: "文本函数",
@@ -204,11 +463,71 @@ export const collapseList = [
   },
   {
     title: "编码",
-    children: [],
+    children: [
+      {
+        title: "ENCODEJSON",
+        doc: "ENCODEJSON({name: 'xiaolin'})",
+        desc: `将JS对象转换成JSON字符串。 示例： ENCODEJSON({name: 'xiaolin'}) 得到 '{"name":"xiaolin"}'。`,
+        children: [
+          {
+            name: "obj",
+            type: "any",
+            desc: "对象或数组",
+          },
+        ],
+      },
+      {
+        title: "DECODEJSON",
+        doc: `DECODEJSON('{\"name\": "xiaolin"}')`,
+        desc: `解析JSON编码数据，返回JS对象。 示例： DECODEJSON('{\"name\": "xiaolin"}') 得到 {name: 'xiaolin'}。`,
+        children: [
+          {
+            name: "str",
+            type: "string",
+            desc: "字符串",
+          },
+        ],
+      },
+    ],
   },
   {
     title: "其他",
-    children: [],
+    children: [
+      {
+        title: "GET",
+        doc: "GET(arr, 2)",
+        desc: "根据对象或者数组的path路径获取值。 如果解析 value 是 undefined 会以 defaultValue 取代。 示例： GET([0, 2, {name: 'xiaolin', age: 18}], 1) 得到 2， GET([0, 2, {name: 'xiaolin', age: 18}], '2.name') 得到 'xiaolin'， GET({arr: [{name: 'xiaolin', age: 18}]}, 'arr[0].name') 得到 'xiaolin'， GET({arr: [{name: 'xiaolin', age: 18}]}, 'arr.0.name') 得到 'xiaolin'， GET({arr: [{name: 'xiaolin', age: 18}]}, 'arr.1.name', 'not-found') 得到 'not-found'。",
+        children: [
+          {
+            name: "obj",
+            type: "any",
+            desc: "对象或数组",
+          },
+          {
+            name: "path",
+            type: "string",
+            desc: "路径",
+          },
+          {
+            name: "defaultValue",
+            type: "any",
+            desc: "如果解析不到则返回该值",
+          },
+        ],
+      },
+      {
+        title: "ISTYPE",
+        doc: "ISTYPE([{a: '1'}, {b: '2'}, {a: '1'}], 'array')",
+        desc: "判断是否为类型支持：string, number, array, date, plain-object。",
+        children: [
+          {
+            name: "判断对象	string",
+            type: "string",
+            desc: "",
+          },
+        ],
+      },
+    ],
   },
 ];
 
