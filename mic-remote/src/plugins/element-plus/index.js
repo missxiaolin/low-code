a39162771cadd7606e136dec3d2dfc5691344828
missxiaolin <462441355@qq.com>
2025-01-03 18:58:30 +0800

    feat: fix

--- mic-remote/src/plugins/element-plus/index.js
+++
@@ -1,9 +0,0 @@
-// import ElementPlus from "element-plus";
-// import zhCn from "element-plus/es/locale/lang/zh-cn";
-
-export function loadElementPlus(app) {
-  /** Element Plus 组件完整引入 */
-  // app.use(ElementPlus, {
-  //   locale: zhCn,
-  // });
-}
