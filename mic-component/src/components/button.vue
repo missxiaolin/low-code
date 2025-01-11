54764db86f657297257eac62f57683ad7c045f64
missxiaolin <462441355@qq.com>
2025-01-11 15:07:31 +0800

    feat: add

---
+++ mic-component/src/components/button.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="css-modules-div">
+    <a-button>Button with style</a-button>
+  </div>
+</template>
+
+<script>
+import { Button } from "ant-design-vue";
+export default {
+  components: {
+    "a-button": Button,
+    setup(props) {},
+  },
+};
+</script>
+<style scoped lang="scss">
+.powderblue {
+  color: powderblue;
+}
+</style>
