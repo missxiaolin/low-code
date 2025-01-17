cf1246f2d3c384ae817b2c33772e1e71c7f0045a
missxiaolin <462441355@qq.com>
2025-01-17 09:43:19 +0800

    feat: add

--- mic-code/src/plugins/ant/index.js
+++ mic-code/src/plugins/ant/index.js
@@ -1,7 +1,11 @@
 import Antd from "ant-design-vue";
+import * as AntdComponent from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
-
+window.Antd = {};
 export function loadAnt(app) {
   /** Ant 组件完整引入 */
   app.use(Antd);
+  for (const [key, component] of Object.entries(AntdComponent)) {
+    window.Antd[key] = component;
+  }
 }
